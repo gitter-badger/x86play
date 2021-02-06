c20cdc1608ea2689768262a2c510a3228e622dc4
2x-1 <76771980+2x-1@users.noreply.github.com>
2021-02-06 22:05:59 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 window.onload = function() {
 	var code = document.getElementById("code");
+	var output = document.getElementById("output");
+	var input = document.getElementById("input");
 	var CCode = CodeMirror.fromTextArea(code, {
+		mode: "text/x-ez80",
+		theme: "material",
+		lineNumbers: true,
+		lineWrapping: true,
+		indentWithTabs: true,
+		scrollbarStyle: null
+	});
+	var CInput = CodeMirror.fromTextArea(input, {
+		mode: "text/x-ez80",
+		theme: "material",
+		lineNumbers: true,
+		lineWrapping: true,
+		indentWithTabs: true,
+		scrollbarStyle: null
+	});
+	var COutput = CodeMirror.fromTextArea(output, {
 		mode: "text/x-ez80",
 		theme: "material",
 		lineNumbers: true,
