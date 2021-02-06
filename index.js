f3c8eb87eae26b5eecda48a23b7c39af85715f8a
2x-1 <76771980+2x-1@users.noreply.github.com>
2021-02-06 21:20:35 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 window.onload = function() {
 	var code = document.getElementById("code");
-	var CodMir = CodeMirror(code, {
+	var CCode = CodeMirror.fromTextArea(code, {
 		mode: "text/x-ez80",
 		theme: "material",
 		lineNumbers: true,
 		lineWrapping: true,
-		indentWithTabs: true
+		indentWithTabs: true,
+		scrollbarStyle: null
 	});
 }
