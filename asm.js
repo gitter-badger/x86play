5af2858bd0b8795add16acf645497a14d053beb9
2x-1 <76771980+2x-1@users.noreply.github.com>
2021-02-08 18:36:13 +0800

    Add files via upload

--- asm.js
+++ asm.js
@@ -1,9 +1,16 @@
 function asm(s) {
 	a = s.split("\n")
+	bytecode = []
 	for(i=0;i<a.length;i++) {
 		b = a[i].match(/(([A-Fa-f0-9]+ )*)(.*)(\;.*)/)[3];
-		b=b.replace(/^\s+|\s+$/g, '').replace(/\s{2,}/g," ")
-		console.log(b)
+		b = b.replace(/^\s+|\s+$/g, '').replace(/\s{2,}/g," ")
+		switch (b.split(' ')[0].toUpperCase()){
+			case 'LODSW':
+				bytecode.push(0xAD)
+			default:
+				console.log("other")
+		}
 	}
-	return s
+	console.log(bytecode)
+	return bytecode
 }
