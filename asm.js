08e14d288639c25e976cab002c1b020951a8b41f
2x-1 <76771980+2x-1@users.noreply.github.com>
2021-02-09 11:43:15 +0800

    Add files via upload

--- asm.js
+++ asm.js
@@ -1,3 +1,30 @@
+function Literal(value) {
+	q=value[value.length-1]
+	if(q=="B")
+		x="0b"+value.slice(0,value.length-1)
+	else if(q=="H")
+		x="0x"+value.slice(0,value.length-1)
+	else if(q=="'")
+		x=value.slice(1,3-1).split("").reduce((y,x)=>y*256+x.charCodeAt(0),0)
+	else
+		x=value
+	return eval(x)
+}
+
+function analyze(args) {
+	args = args.replace(", ",",")
+	return args.split(",").map((i) => {
+		if(i[1]=="X")
+			return ["16bit-reg", i]
+		else if(i[1]=="H" || i[1]=="L")
+			return ["8bit-reg", i]
+		else if(i[0]=="[")
+			return ["address", Literal(i.replace("[","").replace("]",""))]
+		else
+			return Literal(i)
+	})
+}
+
 function asm(s) {
 	a = s.toUpperCase().split("\n")
 	bytecode = []
@@ -8,16 +35,13 @@ function asm(s) {
 		} catch {
 			continue
 		}
-		switch (b.split(' ')[0].toUpperCase()){
-			case 'LODSW':
-				bytecode.push(0xAD)
-				break;
-			case 'STD':
-				bytecode.push(0xFD)
-				break;
-			default:
-				console.log("other")
-				break;
+		q = b.split(' ')[0].toUpperCase())
+		if(q == 'LODSW') {
+			bytecode.push(0xAD)
+		} else if (q == 'STD') {
+			bytecode.push(0xFD)
+		} else {
+			console.log("other")
 		}
 	}
 	console.log(bytecode)
