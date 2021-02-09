a6bfbb5654cc8e4076c603e6758999b02e0aba35
2x-1 <76771980+2x-1@users.noreply.github.com>
2021-02-09 10:51:27 +0800

    Fix a bit of bugs

--- asm.js
+++ asm.js
@@ -1,5 +1,5 @@
 function asm(s) {
-	a = s.split("\n")
+	a = s.toUpperCase().split("\n")
 	bytecode = []
 	for(i=0;i<a.length;i++) {
 		b = a[i].match(/(([A-Fa-f0-9]+ )*)(.*)(\;.*)/)[3];
@@ -7,8 +7,13 @@ function asm(s) {
 		switch (b.split(' ')[0].toUpperCase()){
 			case 'LODSW':
 				bytecode.push(0xAD)
+				break;
+			case 'STD':
+				bytecode.push(0xFD)
+				break;
 			default:
 				console.log("other")
+				break;
 		}
 	}
 	console.log(bytecode)
